26146a4cb3432d995a09b5b7f8a8de1188f18c07
jar-us <suraj.sharma3963@gmail.com>
2024-05-02 13:46:19 +0530

    Add application version printing functionality

--- src/main/java/jar/us/springbootreferenceguide/SpringBootReferenceGuideApplication.java
+++ src/main/java/jar/us/springbootreferenceguide/SpringBootReferenceGuideApplication.java
@@ -2,12 +2,22 @@ package jar.us.springbootreferenceguide;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
-@SpringBootApplication
+@SpringBootApplication  // Annotation to mark this class as a Spring Boot application
 public class SpringBootReferenceGuideApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootReferenceGuideApplication.class, args);
-    }
+        // Run the Spring Boot application and get the application context
+        ConfigurableApplicationContext context = SpringApplication.run(SpringBootReferenceGuideApplication.class, args);
 
+        // Get the application class from the context
+        Class<?> applicationClass = context.getBean(SpringBootReferenceGuideApplication.class).getClass();
+
+        // Get the implementation version of the application
+        String implementationVersion = applicationClass.getPackage().getImplementationVersion();
+
+        // Print the application version using System.out.print
+        System.out.print("Application Version: " + implementationVersion + "\n");
+    }
 }
