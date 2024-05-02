177091a6e9eb5f91c51419e01869f89e196463bf
jar-us <suraj.sharma3963@gmail.com>
2024-05-02 13:30:49 +0530

    Base project structure

---
+++ src/main/java/jar/us/springbootreferenceguide/SpringBootReferenceGuideApplication.java
@@ -0,0 +1,13 @@
+package jar.us.springbootreferenceguide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootReferenceGuideApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootReferenceGuideApplication.class, args);
+    }
+
+}
---
+++ src/test/java/jar/us/springbootreferenceguide/SpringBootReferenceGuideApplicationTests.java
@@ -0,0 +1,13 @@
+package jar.us.springbootreferenceguide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootReferenceGuideApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
