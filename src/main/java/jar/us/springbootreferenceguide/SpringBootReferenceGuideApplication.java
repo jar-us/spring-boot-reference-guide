cfcebde7af7e3a878ab075c96f238aae40a67181
jar-us <suraj.sharma3963@gmail.com>
2024-05-03 13:43:08 +0530

    Sample for implementing startup monitoring

--- src/main/java/jar/us/springbootreferenceguide/SpringBootReferenceGuideApplication.java
+++ src/main/java/jar/us/springbootreferenceguide/SpringBootReferenceGuideApplication.java
@@ -2,12 +2,23 @@ package jar.us.springbootreferenceguide;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.metrics.buffering.BufferingApplicationStartup;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.metrics.ApplicationStartup;
 
 @SpringBootApplication
 public class SpringBootReferenceGuideApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootReferenceGuideApplication.class, args);
+        SpringApplication application = new SpringApplication(SpringBootReferenceGuideApplication.class);
+        application.setApplicationStartup(new BufferingApplicationStartup(2048));
+        application.run(args);
     }
 
+    @Bean
+    @ConditionalOnMissingBean
+    public ApplicationStartup applicationStartup() {
+        return new BufferingApplicationStartup(2048);
+    }
 }
